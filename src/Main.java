0deed1c0bd4ec268d9bad0ed7112d3277c4d0590
Никита <morozov.nva@yandex.ru>
2022-11-21 02:08:15 +0300

    1. Добавлены языки в список команд rus\eng (rl\el) 2. Добавлена возможность добавлять несколько чеков сразу и завершать это командой d-done 3. Добавлена команда суммирования s-summing 4. Добавлена команда подведения баланса b-balance

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,22 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void control(List<ArrayList<Double>> a, List<ArrayList<Double>> b, List<ArrayList<Double>> c, String nik, String dim, String ari) {
         Scanner sc = new Scanner(System.in);
-
+        int langForHelp = 0;
         while(true){
-            outHelp();
+            outHelp(langForHelp);
             var g = sc.nextLine();
             if(g.equals("e")) break;
 
             switch (g) {
                 case ("h"):
+                    break;
+                case ("помогите"):
                     break;
                 case ("a"):
                     addListsControl(a, b, c, nik, dim, ari, sc);
@@ -25,6 +28,15 @@ public class Main {
                     break;
                 case("c"): clearLists(a, b, c);
                     break;
+                case("sum"): sumControl(a,b,c,nik,dim,ari);
+                    break;
+                case("rl"): langForHelp = 1;
+                    break;
+                case("el"): langForHelp = 0;
+                    break;
+                case("b"): balance(a,b,c);
+                outLists(a, b, c, nik, dim, ari);
+                    break;
                 default:
                     System.out.println("Нет такой команды, по крайней мере пока. Читай че написано:");
                     break;
@@ -32,41 +44,9 @@ public class Main {
         }
     }
 
-    public static void outHelp(){
-        System.out.println("Список команд: h-help, a-add, s-show, c-clear, e-esc");
-    }
-
-    public static void addListControl(List<ArrayList<Double>> a, List<ArrayList<Double>> b, List<ArrayList<Double>> c, String nik, String dim, String ari, Scanner sc){
-
-        boolean f = true;
-
-        while (true){
-            System.out.println("Массив чеков, допустим Ника");
-            outLists(a, b, c, nik, dim, ari);
-            System.out.println("_______");
-
-            if(f) System.out.println("Введите сумму чека");
-            else System.out.println("Введите еще число или команду d-done");
-            var g = sc.nextLine();
-
-            if(g.equals("d")) break;
-            int j=0;
-            while(j<3) {
-                try {
-                    // a.add(Double.parseDouble(g));
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("Введите число типа Double");
-                    g = sc.nextLine();
-                    j++;
-                }
-                if (j==3) {
-                    System.out.println("Заебал тупить");
-                    break;
-                }
-            }
-            f = false;
-        }
+    public static void outHelp(int lang){
+        if (lang==0) System.out.println("Список команд: h-help, a-add, s-show, c-clear, sum-summing, b-balance, rl-russian commands, e-esc");
+        if (lang==1) System.out.println("Список команд: помогите-помочь, a-внести в общаг, s-фанеру к осмотру, c-убрать с глаз, sum-сложить2+2, b-подвести итоги, el-перейти на англосакский, e-сьебать");
     }
 
     public static void addListControl(List<ArrayList<Double>> arL, List<ArrayList<Double>> b,List<ArrayList<Double>> c, Scanner sc) {
@@ -74,20 +54,26 @@ public class Main {
 
         while (true) {
 
-           if (f) System.out.println("Введите сумму чека: ");
+            if (f) System.out.println("Введите сумму чека: ");
+            else System.out.println("Введите еще один чек или команду d-done");
+
+            var g = sc.nextLine();
+            if(g.equals("d")) break;
+
             int j = 0;
             double ch = 0;
             while (j < 3) {
                 try {
-                    ch = sc.nextDouble();
+                    ch = Double.parseDouble(g);
                     break;
-                } catch (InputMismatchException e) {
+                } catch (NumberFormatException e) {
                     System.out.println("Введите число типа Double");
-                    sc.nextLine();
+                    g = sc.nextLine();
                     j++;
                 }
                 if (j == 3) System.out.println("Заебал тупить");
             }
+            f = false;
             arL.get(0).add(ch / 2);
             arL.get(1).add(ch / 2);
             b.get(0).add(0.0);
@@ -164,10 +150,67 @@ public class Main {
             case ("Arina"): addListControl(c, a, b, sc);
             break;
         }
-        sc.nextLine();
-
 
     }
+
+    public static void sumControl(List<ArrayList<Double>> a, List<ArrayList<Double>> b, List<ArrayList<Double>> c, String nik, String dim, String ari){
+        outLists(a,b,c,nik, dim, ari);
+        System.out.println("_________________Сумма_________________");
+        if (a.get(0).size()==0) {
+            System.out.println("Че считать то? Список пуст, епта");
+            return;
+        }
+
+        System.out.print((Double) a.get(0).stream().mapToDouble(Double::doubleValue).sum() + "   ");
+        System.out.print((Double) a.get(1).stream().mapToDouble(Double::doubleValue).sum() + "  |  ");
+        System.out.print((Double) b.get(0).stream().mapToDouble(Double::doubleValue).sum() + "   ");
+        System.out.print((Double) b.get(1).stream().mapToDouble(Double::doubleValue).sum() + "  |  ");
+        System.out.print((Double) c.get(0).stream().mapToDouble(Double::doubleValue).sum() + "   ");
+        System.out.print((Double) c.get(1).stream().mapToDouble(Double::doubleValue).sum() + "  |  ");
+        System.out.println();
+//System.out.print(c.get(1).stream().collect(Collectors.summingDouble(Double::doubleValue)) + "   "); было так, но айдиа предложила заменить
+    }
+
+    public static void balance(List<ArrayList<Double>> a, List<ArrayList<Double>> b, List<ArrayList<Double>> c){
+        double a0 = a.get(0).stream().mapToDouble(Double::doubleValue).sum();
+        double b0 = b.get(0).stream().mapToDouble(Double::doubleValue).sum();
+        if(a0>b0) {
+            a0 -= b0;
+            b0 = 0;
+        } else {
+            b0 -= a0;
+            a0 = 0;
+        }
+
+        double a1 = a.get(1).stream().mapToDouble(Double::doubleValue).sum();
+        double c0 = c.get(0).stream().mapToDouble(Double::doubleValue).sum();
+        if(a1>c0) {
+            a1 -= c0;
+            c0 = 0;
+        } else {
+            c0 -= a1;
+            a1 = 0;
+        }
+
+        double b1 = b.get(1).stream().mapToDouble(Double::doubleValue).sum();
+        double c1 = c.get(1).stream().mapToDouble(Double::doubleValue).sum();
+        if(b1>c1) {
+            b1 -= c1;
+            c1 = 0;
+        } else {
+            c1-= b1;
+            b1 = 0;
+        }
+        System.out.println(a0 + " " + a1+ " | " +b0 + " " + b1 + " | " + c0 + " " + c1);
+        clearLists(a,b,c);
+        a.get(0).add(a0);
+        a.get(1).add(a1);
+        b.get(0).add(b0);
+        b.get(1).add(b1);
+        c.get(0).add(c0);
+        c.get(1).add(c1);
+    }
+
 
     public static void main(String[] args) throws IOException {
 
@@ -215,4 +258,5 @@ public class Main {
         control(checksNik, checksDima, checksArina, nameNik, nameDima, nameArina);
         
     }
+
 }
